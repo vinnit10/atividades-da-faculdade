19adc41518f32c1de5572a6a8f1f661da0c7ca51
Vinicius Teixeira <137602744+vinnit10@users.noreply.github.com>
2024-02-01 11:31:21 -0300

    adicionando comentarios

--- Atividade pratica 02.java
+++ Atividade pratica 02.java
@@ -3,18 +3,26 @@ import javax.swing.JOptionPane;
 public class Media {
 
     public static void main(String[] args) {
+        // declaração de variáveis e/ou constantes
         int i;
         double[] Num = new double[50];
         double media, soma = 0;
 
+        // processamento de dados
         for (i = 0; i < 50; i++) {
+            // mensagem ao usuário
             System.out.println("Digite um número:");
+
+            // entrada de dados
             Num[i] = Double.parseDouble(JOptionPane.showInputDialog("Digite um número"));
+
             soma += Num[i];
         }
 
+        // processamento de dados
         media = soma / 50;
 
+        // saída de resultados
         System.out.println("A média dos 50 números digitados é: " + media);
     }
 }
